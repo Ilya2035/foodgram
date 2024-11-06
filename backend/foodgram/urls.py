6956fc4323a264490bee2c9c835af0c3b6770877
Ilya Kizilov <i.g.kizilov@yandex.ru>
2024-11-06 12:10:31 +0200

    Alfa 0.1.2

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,6 +1,28 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from recipes.views import RecipeViewSet
+from tags.views import TagViewSet
+from ingredients.views import IngredientViewSet
+from users.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
