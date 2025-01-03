8a5a35e058d977e0a90e197e89cc89e5f73366a1
Ilya Kizilov <i.g.kizilov@yandex.ru>
2025-01-03 13:27:29 +0200

    Alfa 1.3.0

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,8 +7,10 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('tags.urls')),
     path('', include('ingredients.urls')),
