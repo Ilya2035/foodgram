7c9f8b5ccca96c3fb21d50c36d595deb87eed559
Ilya Kizilov <i.g.kizilov@yandex.ru>
2025-01-16 15:57:44 +0200

    Beta 0.9.9

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -150,8 +150,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = os.getenv('DJANGO_MEDIA_URL', '/media/')
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/backend_media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
